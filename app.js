7b41a729bbdd8c51b3f7fb190179c8e15d5ea02c
Kaisei Forman <kaiseiforman@gmail.com>
2022-02-25 22:01:45 +1000

    Signed-off-by: Kaisei Forman <kaiseiforman@gmail.com>

--- app.js
+++ app.js
@@ -109,18 +109,11 @@ function LocatorPlus(configuration) {
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    `<h1 id="firstHeading" class="firstHeading">${location.title + ":"} ${location.title2}</h1>` +
+    `<div id="photopin"> <img class="locaphoto" src=${location.img} alt="Cinque Terre" width="200" height="150"></img></div>` +
+    `<h1 id="firstHeading" class="firstHeading">${location.title}</h1>` +
     '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Heritage Site.</p>" +
+    "<p><b>Available Camps:</b></p>" +
+    `<p>${location.Available}</p>`+
     "</div>" +
     "</div>";
 
--- locations.js
+++ locations.js
@@ -3,12 +3,18 @@ animicon = "img/arrowsmallerorange.png";
 filmicon = "img/arrowsmallerblue.png";
 youtubeicon = "img/arrowsmallerred.png";
 
+locaMirus = "img/MirusAcademy.png";
+locaShady = "img/ShadyOak.png";
+locaTCS = "img/TCS.png"; 
+locaYork = "img/Yorkshire.png"; 
+
+
 const CONFIGURATION = {
     "locations": [
-      {"title":"Katy","title2":"Mirus Academy","address1":"5561 3rd St","address2":"Katy, TX 77493, USA","coords":{"lat":29.787922492451827,"lng":-95.82045536441802},"placeId":"ChIJsVW26iMkQYYRq4rXBj7EgJs","camps":[{camp:"Animation",days:"3 days | Mon-Wed", price:"$229.99",icon:animicon},{camp:"Filmmaking",days:"3 days | Mon-Wed", price:"$229.99",icon:filmicon},{camp:"Youtubers",days:"3 days | Mon-Wed", price:"$229.99",icon:youtubeicon}]},
-      {"title":"Richmond","title2":"Shady Oak Primary","address1":"600 Main St","address2":"Richmond, TX 77469, USA","coords":{"lat":29.579462734973493,"lng":-95.76243063558196},"placeId":"ChIJg4twb3oeQYYRLszePNTxfyo","camps":[{camp:"Animation",days:"3 days | Mon-Wed", price:"$229.99",icon:animicon},{camp:"Filmmaking",days:"3 days | Mon-Wed", price:"$229.99",icon:filmicon},{camp:"Youtubers",days:"3 days | Mon-Wed", price:"$229.99",icon:youtubeicon}]},
-      {"title":"Cy-fair","title2":"Texas Christian Academy","address1":"17810 Kieth Harrow Blvd","address2":"Cypress, TX 77084, USA","coords":{"lat":29.84985717507098,"lng":-95.67134867934465},"placeId":"ChIJETdJFt_ZQIYR2QC-Cb4o2bw","camps":[{camp:"Animation",days:"3 days | Mon-Wed", price:"$229.99",icon:animicon},{camp:"Filmmaking",days:"3 days | Mon-Wed", price:"$229.99",icon:filmicon},{camp:"Youtubers",days:"3 days | Mon-Wed", price:"$229.99",icon:youtubeicon}]},
-      {"title":"Memorial","title2":"Yorkshire Academy","address1":"14120 Memorial Dr","address2":"Houston, TX 77079, USA","coords":{"lat":29.77108864232313,"lng":-95.59252427790985},"placeId":"ChIJfWgquWTbQIYRD-wa36hRBZE","camps":[{camp:"Animation",days:"5 half days | Mon-Fri", price:"$199.99",icon:animicon},{camp:"Filmmaking",days:"5 half days | Mon-Fri", price:"$199.99",icon:filmicon}]},
+      {"title":"Mirus Academy Katy","img":locaMirus,"Available":"Filmmaking, Animation, Youtube Creators","address1":"5561 3rd St","address2":"Katy, TX 77493, USA","coords":{"lat":29.787922492451827,"lng":-95.82045536441802},"placeId":"ChIJsVW26iMkQYYRq4rXBj7EgJs","camps":[{camp:"Animation",days:"3 days | Mon-Wed", price:"$229.99",icon:animicon},{camp:"Filmmaking",days:"3 days | Mon-Wed", price:"$229.99",icon:filmicon},{camp:"Youtubers",days:"3 days | Mon-Wed", price:"$229.99",icon:youtubeicon}]},
+      {"title":"Shady Oak Primary Richmond","img":locaShady,"Available":"Filmmaking, Animation, Youtube Creators","address1":"600 Main St","address2":"Richmond, TX 77469, USA","coords":{"lat":29.579462734973493,"lng":-95.76243063558196},"placeId":"ChIJg4twb3oeQYYRLszePNTxfyo","camps":[{camp:"Animation",days:"3 days | Mon-Wed", price:"$229.99",icon:animicon},{camp:"Filmmaking",days:"3 days | Mon-Wed", price:"$229.99",icon:filmicon},{camp:"Youtubers",days:"3 days | Mon-Wed", price:"$229.99",icon:youtubeicon}]},
+      {"title":"TX Christian School Cy-fair","img":locaTCS,"Available":"Filmmaking, Animation, Youtube Creators","address1":"17810 Kieth Harrow Blvd","address2":"Cypress, TX 77084, USA","coords":{"lat":29.84985717507098,"lng":-95.67134867934465},"placeId":"ChIJETdJFt_ZQIYR2QC-Cb4o2bw","camps":[{camp:"Animation",days:"3 days | Mon-Wed", price:"$229.99",icon:animicon},{camp:"Filmmaking",days:"3 days | Mon-Wed", price:"$229.99",icon:filmicon},{camp:"Youtubers",days:"3 days | Mon-Wed", price:"$229.99",icon:youtubeicon}]},
+      {"title":"Yorkshire Academy Memorial","img":locaYork,"Available":"Filmmaking, Animation","address1":"14120 Memorial Dr","address2":"Houston, TX 77079, USA","coords":{"lat":29.77108864232313,"lng":-95.59252427790985},"placeId":"ChIJfWgquWTbQIYRD-wa36hRBZE","camps":[{camp:"Animation",days:"", price:"Registration Opens April",icon:animicon},{camp:"Filmmaking",days:"", price:"Registration Opens April",icon:filmicon}]},
     ],
     "mapOptions": {"center":{"lat":38.0,"lng":-100.0},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":false,"zoom":4,"zoomControl":true,"maxZoom":17},
     "mapsApiKey": "AIzaSyDH_VUHgooxpBZJBWEHfHnRt_mcCRsMvyI"
